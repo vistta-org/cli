7a921cb29dc2a350a8d2247d6904ece7a1973734
Tiago Terenas Almeida <tiagoterenas@gmail.com>
2024-09-07 01:29:33 +0100

    #22 & #23

--- cli/test.js
+++ cli/test.js
@@ -3,10 +3,13 @@ import { Console, colors } from "@vistta/console";
 import { default as DefaultCLI } from "./default.js";
 import assert from 'node:assert';
 
+const cwd = process.cwd();
+
 export default class extends DefaultCLI {
-  #suite;
   #results = [];
   #failed;
+  #suite;
+  #runningCounter = 0;
 
   constructor(options) {
     super(options);
@@ -22,27 +25,31 @@ export default class extends DefaultCLI {
   }
 
   help() {
-    system.log("vistta test [pattern]");
+    system.log("vistta test [...patterns]");
     system.log("\nUsage:\n");
-    system.log("vistta test [pattern]\tRuns all the tests that match the pattern in the current project");
+    system.log("vistta test [...patterns]\tRuns all the tests that match the pattern/s in the current project");
   }
 
-  async main(_, pattern = "**/*.test.js") {
+  async main(_, ...patterns) {
     system.announce(`Vistta CLI v${process.env.CLI_VERSION}`);
-    const entries = fs.glob(fs.resolve(process.cwd(), pattern));
+    if (patterns.length === 0) patterns = ["**/*.test.js", "**/*.test.ts"];
+    for (let i = 0, len = patterns.length; i < len; i++)
+      patterns[i] = fs.resolve(cwd, patterns[i])
+    const entries = fs.glob(patterns);
     let entry = (await entries.next())?.value;
     while (entry) {
       try {
         if (!entry.includes("\\node_modules\\")) await import(fs.resolve(entry));
+        await this.#await();
       }
       catch { this.#failed = true; }
       entry = (await entries.next())?.value;
     }
     let output = "";
     for (let i = 0, len = this.#results.length; i < len; i++) {
-      const { name, tests, time } = this.#results[i];
+      const { name, tests } = this.#results[i];
       if (name) output += `\n${name}\n`;
-      const [results, passing, total] = processTests(tests);
+      const [results, passing, total, time] = processTests(tests);
       output += `${results}${passing === total ? colors.green : colors.red}${passing}/${total} passing ${colors.reset + colors.dim}(${time}ms)${colors.reset}\n`
     }
     system.log(output);
@@ -51,47 +58,46 @@ export default class extends DefaultCLI {
 
   async suite(name, callback) {
     if (this.#suite) throw new Error("Suites/Describes cannot be stacked");
+    this.#runningCounter++;
     this.#suite = { name, tests: [] };
-    const start = performance();
     try {
       const value = callback();
-      if (value instanceof Promise)
-        value.then(() => this.#suite.time = performance(start))
-          .catch(() => this.#suite.time = performance(start));
-      else this.#suite.time = performance(start)
-    } catch {
-      this.#suite.time = performance(start)
-    }
+      if (value instanceof Promise) await value;
+    } catch { /* Do Nothing */ }
     this.#results.push(this.#suite);
     this.#suite = null;
+    this.#runningCounter--;
   }
 
   async test(name, callback) {
+    this.#runningCounter++;
     const test = { name };
     const start = performance();
+    if (this.#suite) this.#suite.tests.push(test);
+    else this.#results.push({ tests: [test], time: test.time });
+
     try {
       const value = callback();
-      if (value instanceof Promise) value.then(() => {
-        test.status = "pass";
-        test.time = performance(start);
-      }).catch((error) => {
-        test.status = "fail";
-        test.error = error;
-        test.time = performance(start);
-        this.#failed = true;
-      });
-      else {
-        test.status = "pass";
-        test.time = performance(start);
-      }
+      if (value instanceof Promise) await value
+      test.status = "pass";
+      test.time = performance(start);
     } catch (error) {
       test.status = "fail";
       test.error = error;
       test.time = performance(start);
       this.#failed = true;
     }
-    if (this.#suite) this.#suite.tests.push(test);
-    else this.#results.push({ tests: [test], time: test.time });
+    this.#runningCounter--;
+  }
+
+  #await() {
+    return new Promise((resolve) => {
+      const timer = () => {
+        if (this.#runningCounter === 0) resolve();
+        else setTimeout(timer, 500);
+      }
+      timer();
+    });
   }
 }
 
@@ -104,9 +110,11 @@ function performance(start) {
 function processTests(tests) {
   let passing = 0;
   let total = 0;
+  let timeAcc = 0;
   let acc = "";
   for (let i = 0, len = tests?.length || 0; i < len; i++) {
     const { name, status, time, error } = tests[i];
+    timeAcc += time;
     if (status === "pass") {
       acc += `  ${colors.green}✔  ${name} ${colors.reset + colors.dim}(${time}ms)${colors.reset}\n`;
       passing++;
@@ -114,5 +122,5 @@ function processTests(tests) {
     else acc += `  ${colors.red}✖  ${name} ${colors.reset + colors.dim}(${time}ms)${colors.reset}\n\t${colors.red + error + colors.reset}\n`;
     total++;
   }
-  return [acc, passing, total];
+  return [acc, passing, total, timeAcc];
 }
